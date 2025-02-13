e0236919fb7907b707f67daffc09e37953c2160c
Guilherme48Dev <guilhermesouzadjesusdev@gmail.com>
2025-02-13 03:19:59 -0300

    styled components

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,12 +1,24 @@
-import "./Logo.css"
-import logo from "../../imagens/logo.png"
+import logo from "../../imagens/logo.png";
+import styled from "styled-components";
 
-function Logo () {
-    return(
-        <div className="Logo">
-          <img src={logo} alt="Logo do site isBook" className="logo-img"/>
-        </div>
-    )
+const LogoCtn = styled.div`
+  flex: 1;
+  display: flex;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+const LogoImg = styled.img`
+  width: 140px;
+  height: 140px;
+`;
+
+function Logo() {
+  return (
+    <LogoCtn>
+      <LogoImg src={logo} alt="Logo do site isBook" className="logo-img" />
+    </LogoCtn>
+  );
 }
 
 export default Logo;
